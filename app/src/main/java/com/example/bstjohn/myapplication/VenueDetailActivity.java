ce96039849831de78cd9440efae6bf93f446e8e1
Brendan StJohn <bstjohn@gilt.com>
2015-05-26 15:55:37 +0100

    Wire components with butterknife

--- app/src/main/java/com/example/bstjohn/myapplication/VenueDetailActivity.java
+++ app/src/main/java/com/example/bstjohn/myapplication/VenueDetailActivity.java
@@ -1,38 +1,60 @@
 package com.example.bstjohn.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 
 public class VenueDetailActivity extends Activity {
+
+    @InjectView(R.id.detail_image)
+    ImageView detailsImage;
+
+    @InjectView((R.id.detail_text))
+    TextView detailText;
+
+
+    @InjectView((R.id.detail_description))
+    TextView description;
+
+    @InjectView((R.id.detail_open_in_browser))
+    Button btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_venue_detail);
+        ButterKnife.inject(this);
+        initButtons();
+        initImage();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_venue_detail, menu);
-        return true;
+    private void initImage() {
+        Picasso.with(this).load("http://i.imgur.com/DvpvklR.png").into(detailsImage);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    private void initButtons() {
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://www.gilt.com"));
+                startActivity(intent);
+            }
+        });
     }
+
 }
